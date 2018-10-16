4e84d9a9fc3ff874e130c3210f6d2e77379ed201
943804215 <943804215@qq.com>
2018-10-16 15:30:04 +0800

    第一次提交瀑布流

---
+++ dist/js/nav.js
@@ -0,0 +1,134 @@
+$('.hz').hover(function(){
+    $(this).find('.menu2').show();
+    $(this).find('.dao').css({"border":"3px solid #dddddd","border-bottom":0,"background":"#fff","position":"relative","z-index":"10"});
+    $(this).find('.menu2').css("background","#fff")
+},function(){
+    $(this).find('.menu2').hide();
+    $(this).find('.dao').css({"border":"none","background":0})
+})
+
+
+
+$('.cel').hover(function(){
+    $(this).find('.erweima').show();
+    $(this).find('.icon').css({"border":"3px solid #dddddd","border-bottom":"0","background":"#fff","position":"relative","z-index":"10"})
+},function(){
+    $(this).find('.erweima').hide();
+    $(this).find('.icon').css({"border":"none","background":0})
+})
+
+
+$(".hang1").hover(function(){
+    $(this).find(".ul1-1").show();
+    $(this).find(".one").css({"background":"#88766e","color":"#fff","font-weight":"bold","position":"relative","z-index":"11"});
+    $(this).find("p").css({"color":"#fff","font-weight":"bold","position":"relative","z-index":"11"});
+    // $(this).find(".ul1-1 a").css({"color":"#d82929"});
+},function(){
+    $(this).find(".ul1-1").hide();
+    $(this).find(".one").css({"background":0});
+    $(this).find("p").css({"color":"#6b6767","font-weight":"normal"});
+    // $(this).find(".ul1-1 a").css({"color":"#888888"});
+})
+
+
+$(".hang2").hover(function(){
+    $(this).find(".ul1-2").show();
+    $(this).find(".two").css({"background":"#88766e","color":"#fff","font-weight":"bold","position":"relative","z-index":"11"});
+    $(this).find("p").css({"color":"#fff","font-weight":"bold","position":"relative","z-index":"11"});
+},function(){
+    $(this).find(".ul1-2").hide();
+    $(this).find(".two").css({"background":0});
+    $(this).find("p").css({"color":"#6b6767","font-weight":"normal"});
+})
+
+
+$(".hang3").hover(function(){
+    $(this).find(".ul1-3").show();
+    $(this).find(".three").css({"background":"#88766e","color":"#fff","font-weight":"bold","position":"relative","z-index":"11"});
+    $(this).find("p").css({"color":"#fff","font-weight":"bold","position":"relative","z-index":"11"});
+},function(){
+    $(this).find(".ul1-3").hide();
+    $(this).find(".three").css({"background":0});
+    $(this).find("p").css({"color":"#6b6767","font-weight":"normal"});
+})
+
+$(".hang4").hover(function(){
+    $(this).find(".ul1-4").show();
+    $(this).find(".one").css({"background":"#88766e","color":"#fff","font-weight":"bold","position":"relative","z-index":"11"});
+    $(this).find("p").css({"color":"#fff","font-weight":"bold","position":"relative","z-index":"11"});
+    // $(this).find(".ul1-1 a").css({"color":"#d82929"});
+},function(){
+    $(this).find(".ul1-4").hide();
+    $(this).find(".one").css({"background":0});
+    $(this).find("p").css({"color":"#6b6767","font-weight":"normal"});
+    // $(this).find(".ul1-1 a").css({"color":"#888888"});
+})
+
+
+
+$(".hang5").hover(function(){
+    $(this).find(".ul1-5").show();
+    $(this).find(".one").css({"background":"#88766e","color":"#fff","font-weight":"bold","position":"relative","z-index":"11"});
+    $(this).find("p").css({"color":"#fff","font-weight":"bold","position":"relative","z-index":"11"});
+    // $(this).find(".ul1-1 a").css({"color":"#d82929"});
+},function(){
+    $(this).find(".ul1-5").hide();
+    $(this).find(".one").css({"background":0});
+    $(this).find("p").css({"color":"#6b6767","font-weight":"normal"});
+    // $(this).find(".ul1-1 a").css({"color":"#888888"});
+})
+
+$(".hang6").hover(function(){
+    $(this).find(".ul1-6").show();
+    $(this).find(".one").css({"background":"#88766e","color":"#fff","font-weight":"bold","position":"relative","z-index":"11"});
+    $(this).find("p").css({"color":"#fff","font-weight":"bold","position":"relative","z-index":"11"});
+    // $(this).find(".ul1-1 a").css({"color":"#d82929"});
+},function(){
+    $(this).find(".ul1-6").hide();
+    $(this).find(".one").css({"background":0});
+    $(this).find("p").css({"color":"#6b6767","font-weight":"normal"});
+    // $(this).find(".ul1-1 a").css({"color":"#888888"});
+})
+
+$(".hang7").hover(function(){
+    $(this).find(".ul1-7").show();
+    $(this).find(".two").css({"background":"#88766e","color":"#fff","font-weight":"bold","position":"relative","z-index":"11"});
+    $(this).find("p").css({"color":"#fff","font-weight":"bold","position":"relative","z-index":"11"});
+},function(){
+    $(this).find(".ul1-7").hide();
+    $(this).find(".two").css({"background":0});
+    $(this).find("p").css({"color":"#6b6767","font-weight":"normal"});
+})
+
+
+$(".hang8").hover(function(){
+    $(this).find(".ul1-8").show();
+    $(this).find(".one").css({"background":"#88766e","color":"#fff","font-weight":"bold","position":"relative","z-index":"11"});
+    $(this).find("p").css({"color":"#fff","font-weight":"bold","position":"relative","z-index":"11"});
+    // $(this).find(".ul1-1 a").css({"color":"#d82929"});
+},function(){
+    $(this).find(".ul1-8").hide();
+    $(this).find(".one").css({"background":0});
+    $(this).find("p").css({"color":"#6b6767","font-weight":"normal"});
+    // $(this).find(".ul1-1 a").css({"color":"#888888"});
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ dist/js/pubuliu.js
@@ -0,0 +1,95 @@
+
+            function Pagination(){
+
+            }
+            Pagination.prototype.init=function(){
+                this.wrap=document.querySelector("#wrap1 ul");
+                this.clientHeight=document.documentElement.clientHeight;
+                this.loading=false;
+                this.now_page=0;
+                this.loadJson()
+                .then(function(json){
+                    this.data=json;
+                    this.total=Math.ceil(json.count/4);
+                    this.renderPage();
+                }.bind(this));
+                this.handleEvent();
+            }
+            Pagination.prototype.loadJson=function(){
+                return new Promise(function(success,error){
+                    var xhr=new XMLHttpRequest();
+                    xhr.open("GET","http://localhost:8888/proxy/api.douban.com/v2/movie/top250");
+                    xhr.send(null);
+                    xhr.onload=function(){
+                        if(xhr.status==200){
+                            var json=typeof xhr.response=="string"?JSON.parse(xhr.response):xhr.response;
+                            success(json)
+
+                        }else{
+                            error("请求出错")
+                        }    
+                    }
+                })
+            }
+            Pagination.prototype.renderPage=function(){
+                //渲染页面
+                var list=this.data.subjects;
+                var html="";
+                 // 起始点和终止点;
+                // 第几页   	范围 	for循环起始点	for循环终止点
+                // 0	        0~3	            0	        3
+                // 1	        4~7	            4	        7
+                // 2	        8~11	        8	        11
+                // 3	        12~15	        12	        15
+                // 4	        16~19	        16	        19
+                // 5	        20~23	        20	        23
+                // 6	        24~27	        24	        27
+                // n	                     4 * n       	4 * n  + 3
+                for(var i=4*this.now_page;i<=4*this.now_page+3;i++){
+                    html+=` 
+                            <li>
+                                <img src="${list[i].images.small}" alt="">
+                                <p><a href="#">${list[i].title}</a></p>
+                                <p>${getName(list[i].casts)} ${getName(list[i].directors)}</p>
+                                <p>${list[i].year}</p>
+                                <p>${list[i].rating.average}</p>
+                                <div class="shop">
+                                    <a href="#">加入购物车</a>
+                                </div>
+                            </li>`
+                }
+                this.wrap.innerHTML+=html;
+                this.loading=false;
+            }
+            Pagination.prototype.handleEvent=function(){
+                onscroll=this.load.bind(this)
+            }
+            Pagination.prototype.load=function(){
+                if(this.loading){
+                    return 0;
+                }
+                var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+                var showHeight = scrollTop + this.clientHeight;
+                var eleChildren = this.wrap.getElementsByTagName("li");
+                var lastChildren = eleChildren[eleChildren.length - 1];
+                var lastTop = lastChildren.offsetTop;
+                if(lastTop <= showHeight){
+                // 加载数据;
+                // 加载过程之中 不重复加载;
+                // console.log("加载数据");
+                this.loading = true;
+                this.now_page ++;  
+
+                if(this.now_page > this.total){
+                    return 0;
+                }
+
+                this.renderPage();
+                // document.documentElement.scrollTop = 0;
+                // console.log(1);
+            }
+
+
+            }
+            var pagination=new Pagination();
+            pagination.init()
